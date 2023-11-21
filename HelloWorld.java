3989edc50e41a39e38fd8cb3308562dabb1394d5
jmstechhome25 <jmstechhome25@gmail.com>
2023-11-21 02:02:25 +0000

    added hello devops

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
-public class HelloWorld {
+public class HelloDevOps {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
+    System.out.println("Hello, DevOps");
+    System.out.println("Hello, DevOps");
+    System.out.println("Hello, DevOps");
+    System.out.println("Hello, DevOps");
   }
 }
